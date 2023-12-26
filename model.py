ea66d7615a89e4678c84d49893b116b242b23828
dpkeee <itzdeepika@gmail.com>
2023-12-26 09:22:32 +0000

    Modular code

--- model.py
+++ model.py
@@ -1,53 +1,104 @@
-# Train data transformations
-train_transforms = transforms.Compose([
-    transforms.RandomApply([transforms.CenterCrop(22), ], p=0.1),
-    transforms.Resize((28, 28)),
-    transforms.RandomRotation((-15., 15.), fill=0),
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    ])
+import torch.nn as nn
+import torch.nn.functional as F
 
-# Test data transformations
-test_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1407,), (0.4081,))
-    ])
-
-train_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
-test_data = datasets.MNIST('../data', train=True, download=True, transform=train_transforms)
-
-batch_size = 512
-
-kwargs = {'batch_size': batch_size, 'shuffle': False, 'num_workers': 2, 'pin_memory': True}
-
-test_loader = torch.utils.data.DataLoader(train_data, **kwargs)
-train_loader = torch.utils.data.DataLoader(train_data, **kwargs)
+from torchsummary import summary
+import tqdm
+from utils import GetCorrectPredCount
+import matplotlib.pyplot as plt
 
 class Net(nn.Module):
     #This defines the structure of the NN.
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=False)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=False)
+        self.fc1 = nn.Linear(4096, 50, bias=False)
+        self.fc2 = nn.Linear(50, 10, bias=False)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x), 2)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2)) 
-        x = F.relu(self.conv3(x), 2)
-        x = F.relu(F.max_pool2d(self.conv4(x), 2)) 
-        x = x.view(-1, 320)
+        x = F.relu(self.conv1(x), 2) # 28>26 | 1>3 | 1>1
+        x = F.relu(F.max_pool2d(self.conv2(x), 2)) #26>24>12 | 3>5>6 | 1>1>2
+        x = F.relu(self.conv3(x), 2) # 12>10 | 6>10 | 2>2
+        x = F.relu(F.max_pool2d(self.conv4(x), 2)) # 10>8>4 | 10>14>16 | 2>2>4
+        x = x.view(-1, 4096) # 4*4*256 = 4096
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
-# Data to plot accuracy and loss graphs
+def model_summary(model,input_size):
+    summary(model, input_size)    
+
 train_losses = []
 test_losses = []
 train_acc = []
 test_acc = []
 
 test_incorrect_pred = {'images': [], 'ground_truths': [], 'predicted_vals': []}
+
+def model_train(model, device, train_loader, optimizer, criterion):
+  model.train()
+  #pbar = tqdm(train_loader)
+
+  train_loss = 0
+  correct = 0
+  processed = 0
+
+  for batch_idx, (data, target) in enumerate(train_loader):
+    data, target = data.to(device), target.to(device)
+    optimizer.zero_grad()
+
+    # Predict
+    pred = model(data)
+
+    # Calculate loss
+    loss = criterion(pred, target)
+    train_loss+=loss.item()
+
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    
+    correct += GetCorrectPredCount(pred, target)
+    processed += len(data)
+
+    #pbar.set_description(desc= f'Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+
+  train_acc.append(100*correct/processed)
+  train_losses.append(train_loss/len(train_loader))
+
+def model_test(model, device, test_loader, criterion):
+    model.eval()
+
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data, target = data.to(device), target.to(device)
+
+            output = model(data)
+            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+
+            correct += GetCorrectPredCount(output, target)
+
+
+    test_loss /= len(test_loader.dataset)
+    test_acc.append(100. * correct / len(test_loader.dataset))
+    test_losses.append(test_loss)
+
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    
+def draw_graphs(train_loss,train_acc,test_loss,test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
